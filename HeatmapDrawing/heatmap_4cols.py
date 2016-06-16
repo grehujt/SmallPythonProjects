d7acfdbceaa0d28b95044a4f760c83588d260283
grehujt <kidd4212@gmail.com>
2016-06-16 15:54:50 +0800

    heatmap demo init

---
+++ HeatmapDrawing/heatmap_4cols.py
@@ -0,0 +1,49 @@
+
+from __future__ import division
+
+import numpy as np
+from matplotlib import cm
+import matplotlib.mlab as ml
+
+import pylab as pl
+from pylab import imread
+
+import pandas as pd
+
+
+projectFolder = r'./data'
+imFile = '%s/bg.png' % projectFolder
+dataFile = '%s/data.csv' % projectFolder
+outImage = './output.png'
+
+
+def plot_heatmap(bgImFile, txt, outImage):
+    mat = pd.read_csv(txt, header=None)
+    im = imread(imFile)
+    image_height, image_width = im.shape[:2]
+    num_x = image_width / 5
+    num_y = num_x / (image_width / image_height)
+    x = np.linspace(0, image_width, num_x)
+    y = np.linspace(0, image_height, num_y)
+
+    figure = pl.figure(figsize=(10, 10), dpi=100)
+    ax = pl.subplot(211)
+    z = ml.griddata(mat[0], mat[1], mat[2], x, y)
+    cs = ax.contourf(x, y, z, alpha=0.6, zorder=1, cmap=cm.jet)
+    pl.colorbar(cs)
+    pl.plot(mat[0], mat[1], '+', alpha=0.6, markersize=1.5, zorder=3)
+    ax.imshow(im, alpha=0.3, zorder=0)
+
+    ax = pl.subplot(212)
+    z = ml.griddata(mat[0], mat[1], mat[3], x, y)
+    cs = ax.contourf(x, y, z, alpha=0.6, zorder=1, cmap=cm.jet)
+    pl.colorbar(cs)
+    pl.plot(mat[0], mat[1], '+', alpha=0.6, markersize=1.5, zorder=3)
+    ax.imshow(im, alpha=0.3, zorder=0)
+
+    pl.savefig(outImage)
+    pl.clf()
+    pl.close(figure)
+
+if __name__ == '__main__':
+    plot_heatmap(imFile, dataFile, outImage)
---
+++ HeatmapDrawing/readme.md
@@ -0,0 +1,23 @@
+# Drawing heatmaps based on csv file
+
+### Input
+- ./data/data.csv
+    + contains no headers
+    + line format: "x,y,v1,v2"
+- ./data/bg.png: the background image file
+
+### Output
+- output.png, which is the heatmap
+
+### program files
+- heatmap_3cols.py:
+    + takes the first 3 cloumns (x,y,v1) to draw the heatmap
+
+    ![demo1](./pic/demo1.png)
+
+- heatmap_4cols.py:
+    + will output 2 subgraphs
+    + draw two heatmaps, one for (x,y,v1)
+    + the other for (x,y,v2)
+
+    ![demo2](demo2.png)
