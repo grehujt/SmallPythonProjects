e93ade966099c29d5238a7b9a866cee9ad656e26
grehujt <grehujt@users.noreply.github.com>
2018-10-19 16:21:46 +0800

    Update contrast.py

--- image_processing/contrast.py
+++ image_processing/contrast.py
@@ -1,6 +1,6 @@
 import cv2
 im = cv2.imread('testim.png')
-im2 = (((im2/255.0)**0.37)*255).astype('uint8')
+im2 = (((im/255.0)**0.37)*255).astype('uint8')
 cv2.imshow('im', im)
 cv2.imshow('im2', im2)
 cv2.waitKey(0)
